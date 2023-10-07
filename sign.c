c3792c96424f55d26d1b1d6e0f3e701520d397c2
tony <tchow@tutamail.com>
2023-10-07 14:11:41 -0400

    sign of an integer (-1, 0, 1)

---
+++ sign.c
@@ -0,0 +1,24 @@
+#include <limits.h>
+#include <stdbool.h>
+
+bool sign_ref(int x) {
+  if (x < 0) {
+    return -1;
+  } else if (x > 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+bool sign_bit(int x) {
+  return (x != 0) | (x >> (sizeof(int) * CHAR_BIT - 1)); // -1, 0, or +1
+}
+
+int main() {
+  int x = nondet_int();
+  bool res_ref = sign_ref(x);
+  bool res_bit = sign_bit(x);
+  CPROVER_assert(res_ref == res_bit,
+                 "Err: sign_bit did not return the same value as sign_ref");
+}
