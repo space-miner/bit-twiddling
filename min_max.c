576e59bc1620969a7d27b9ffec84807fa9634377
tony <tchow@tutamail.com>
2023-10-07 14:35:41 -0400

    remove min.c and max.c and combined them into min_max.c

---
+++ min_max.c
@@ -0,0 +1,36 @@
+int min_ref(int x, int y) {
+  if (x <= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+int min_bit(int x, int y) {
+  return y ^ ((x ^ y) & -(x < y)); // min(x, y)
+}
+
+int max_ref(int x, int y) {
+  if (x <= y) {
+    return y;
+  } else {
+    return x;
+  }
+}
+
+int max_bit(int x, int y) {
+  return x ^ ((x ^ y) & -(x < y)); // max(x, y)
+}
+
+int main() {
+  int x = nondet_int();
+  int y = nondet_int();
+  int minimum_ref = min_ref(x, y);
+  int minimum_bit = min_bit(x, y);
+  int maximum_ref = max_ref(x, y);
+  int maximum_bit = max_bit(x, y);
+  CPROVER_assert(minimum_ref == minimum_bit,
+                 "Err: min_bit did not return the same value as min_ref");
+  CPROVER_assert(maximum_ref == maximum_bit,
+                 "Err: max_bit did not return the same value as max_ref");
+}
