cfa5fba8dd349d6ae75035ce9ad0f4aee6ec9855
tony <tchow@tutamail.com>
2023-10-24 16:25:31 -0400

    finish conditional negate

---
+++ conditional_negate.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+
+int nondet_int();
+bool nondet_bool();
+
+// bool fNegate;  // Flag indicating if we should negate v.
+// int x;         // Input value to negate if fNegate is true.
+// int r;         // result = fNegate ? -v : v;
+
+int conditional_negate_ref(bool fNegate, int x) { return fNegate ? -x : x; }
+
+int conditional_negate_bit(bool fNegate, int x) {
+  return (x ^ -fNegate) + fNegate;
+}
+
+int main() {
+  int x = nondet_int();
+  bool fNegate = nondet_bool();
+  unsigned int res_ref = conditional_negate_ref(fNegate, x);
+  unsigned int res_bit = conditional_negate_bit(fNegate, x);
+  __CPROVER_assert(
+      res_ref == res_bit,
+      "Err: conditional_negate_bit did not return the same value as "
+      "conditional_negate_ref");
+}
